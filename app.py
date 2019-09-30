f6e8f2bef693c111afa9c28d4575e9369c953a2a
uyennguyen16900 <uyennguyen16900@gmail.com>
2019-09-30 12:52:09 -0700

    Users can delete comments

--- app.py
+++ app.py
@@ -84,5 +84,12 @@ def playlists_show(playlist_id):
     playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
